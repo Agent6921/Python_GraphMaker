c1f9a3c5e4ba00b32895543d504c9e7ce3cfebbc
bob o'neil <122388954+Agent6921@users.noreply.github.com>
2023-01-18 21:09:49 +0000

    Add files via upload
    New loop function change the amount of bars by changing the value inside the brakets "range( ):

---
+++ GraphMaker_V2.py
@@ -0,0 +1,22 @@
+import turtle
+t = turtle.Turtle()
+t.speed(5) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+t.up()
+t.left(180)
+t.forward(255)
+t.right(90)
+t.right(180)
+t.forward(300)
+t.right(180)
+t.down()
+
+for i in range(5):  
+  value1 = input()
+  t.forward(value1)
+  t.right(90)
+  t.forward(50)
+  t.right(90)
+  t.forward(value1)
+  t.right(180)
+
+# Agent6921
